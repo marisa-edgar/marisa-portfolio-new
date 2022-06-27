b74384fce1fe0ada40f4031975e08da08715dcd0
marisa-edgar <92288470+marisa-edgar@users.noreply.github.com>
2022-06-26 23:20:09 -0700

    initial commit

---
+++ src/components/Directory/Directory.tsx
@@ -0,0 +1,10 @@
+
+
+export const Directory = () => {
+  return (
+    <>
+    Hello World.<br/>
+    I am Marisa's HomePage.
+    </>
+  )
+}
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import { Directory } from "../../components/Directory/Directory";
+import './styles.css'
+
+const HomePage = () => {
+  return (
+    <section className='homepage'>
+      <Directory />
+    </section>
+  )
+}
+
+export default HomePage;
